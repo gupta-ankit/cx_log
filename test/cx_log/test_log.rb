daad8ee959356b8b7dd63298e5ee214676c40c50
Ankit Gupta <ankit.gupta2801@gmail.com>
2024-06-22 09:23:03 -0700

    fetch a new copy of log for every test

--- test/cx_log/test_log.rb
+++ test/cx_log/test_log.rb
@@ -3,39 +3,39 @@
 require "test_helper"
 
 class TestLog < Minitest::Test
+
+  def setup
+    @log = Class.new(CxLog::Log).instance
+  end
+
   def test_that_it_can_override_message
-    cx_log = CxLog::Log.instance
-    cx_log.add(message: "hello")
-    assert_equal({ message: "hello" }, cx_log.context)
+    @log.add(message: "hello")
+    assert_equal({ message: "hello" }, @log.context)
   end
 
   def test_that_it_can_add_an_event
-    cx_log = CxLog::Log.instance
-    cx_log.add(foo: "bar")
-    assert_equal({ message: "", foo: "bar" }, cx_log.context)
+    @log.add(foo: "bar")
+    assert_equal({ message: "", foo: "bar" }, @log.context)
   end
 
   def test_that_it_can_append
-    cx_log = CxLog::Log.instance
-    cx_log.add(foo: "bar")
-    cx_log.add(foo: "baz")
-    assert_equal({ message: "", foo: ["bar", "baz"] }, cx_log.context)
+    @log.add(foo: "bar")
+    @log.add(foo: "baz")
+    assert_equal({ message: "", foo: ["bar", "baz"] }, @log.context)
   end
 
   def that_it_can_clear
-    cx_log = CxLog::Log.instance
-    cx_log.add(foo: "bar")
-    cx_log.clear
-    assert_equal({ message: "" }, cx_log.context)
+    @log.add(foo: "bar")
+    @log.clear
+    assert_equal({ message: "" }, @log.context)
   end
 
   def test_that_it_can_flush
     logger = Minitest::Mock.new
     logger.expect :info, nil, [{ message: "", foo: "bar" }.to_json]
 
-    cx_log = CxLog::Log.instance
-    cx_log.add(foo: "bar")
-    cx_log.flush(logger)
+    @log.add(foo: "bar")
+    @log.flush(logger)
 
     logger.verify
   end
